e3a50d0d0e14409a5a93190707b7fce240941f94
hariharihari123 <71631538+hariharihari123@users.noreply.github.com>
2021-01-31 12:33:29 +0530

    Add files via upload

---
+++ hi/sketch.js
@@ -0,0 +1,9 @@
+function setup() {
+  createCanvas(displayWidth,displayHeight);
+}
+
+function draw() {
+  background(0);  
+  drawSprites();
+  text('hi i am boared this was easy to make lol i am so boared' ,displayWidth/2,displayHeight/2);
+}
